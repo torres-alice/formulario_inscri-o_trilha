235751fe659d8b2d0481e6b513c7a9377e926508
Alice Torres <torresalice.dev@gmail.com>
2025-03-27 15:43:06 -0300

    validando cpf parte 2

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,10 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Por favor, insira um CPF válido")
+    } else {
+        console.log("CPF existe")
+    }
 }
 function validaNumerosRepetidos(cpf) {
     const validaNumerosRepetidos = [
@@ -36,3 +39,19 @@ function validaPrimeiroDigito(cpf) {
     return soma != cpf[9];
 }
 
+function validaSegundoDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 11;
+
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
+  return soma != cpf[10];
+}
